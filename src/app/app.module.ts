0fa598a987842b9894acb3656830000f5e7d1833
Student <example@example.com>
2019-04-08 15:36:07 -0700

    add initial project setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { SplashComponent } from './splash/splash.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryDetailComponent } from './category-detail/category-detail.component';
+import { ResultsComponent } from './results/results.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SigninComponent } from './signin/signin.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SplashComponent,
+    CategoriesComponent,
+    CategoryDetailComponent,
+    ResultsComponent,
+    ConfirmationComponent,
+    RegistrationComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
