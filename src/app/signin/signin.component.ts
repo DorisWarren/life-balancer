03637702f4503c825810094c7a33aa4f42ea90ae
Student <example@example.com>
2019-04-09 14:23:10 -0700

    add routing based on database email check

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UserInfoService } from '../user-info.service';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { CategoriesComponent } from '../categories/categories.component';
+
+
 
 
 @Component({
@@ -9,10 +13,43 @@ import { CategoriesComponent } from '../categories/categories.component';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  signInForm = new FormGroup ({
+    email: new FormControl(''),
+  });
 
-  constructor() { }
+  resultsFromDatabase;
+
+
+  constructor(private userInfoService: UserInfoService, private routes: Router) { }
 
   ngOnInit() {
+    this.getResults();
+}
+
+getResults = () =>
+  this.userInfoService.getUserByName().subscribe(res => (this.resultsFromDatabase = res));
+
+  onSubmit() {
+    let routes = this.routes;
+    //event emitter or update database here
+    let emailInput = this.signInForm.value;
+    console.log(emailInput);
+    // console.log(this.resultsFromDatabase);
+
+    this.resultsFromDatabase.forEach(function(entry) {
+      console.log(entry.payload.doc.data().email);
+      if (emailInput.email === entry.payload.doc.data().email) {
+        routes.navigate(['results']);
+        // console.log('if you got this far, you should get results');
+      }
+      else {
+        routes.navigate(['register']);
+        // console.log('if you see this, you should go to registration');
+      }
+
+
+    });
+
   }
 
 }
