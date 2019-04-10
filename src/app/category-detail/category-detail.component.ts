a70e6cc3916240fff228f55cb0010614765cea21
Doris Warren <doriswarren11@gmail.com>
2019-04-10 13:07:14 -0700

    add four loop to categories-detail page

--- src/app/category-detail/category-detail.component.ts
+++ src/app/category-detail/category-detail.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConfirmationComponent } from '../confirmation/confirmation.component';
-
+import {categoriesDetail} from '../models/categories-detail.model'
 
 @Component({
   selector: 'app-category-detail',
   templateUrl: './category-detail.component.html',
   styleUrls: ['./category-detail.component.scss']
 })
-export class CategoryDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CategoryDetailComponent   {
+  categories: Category[] = [
+  new Category("Social", "Below are three questions to help self assess your social life ",
+      "How often do you get together with friends outside of work?","Do you talk with or visit family members, other than during the holidays?","Do you feel safe and respected in your primary intimate relationship?", 1),
+  new Category("Work", "Self assessing your work enviroment.",
+      "How much autonomy do you have in performing your job?","Do you feel challenged in your job?","Do you feel supported by your managment? ", 2),
+  new Category("Financial", "Self assessing your finances.",
+      "Have you set and achieved your financial goals?", 3),
+  new Category("Wellness", "Self assessing Health, Fitness, Mindfulness!",
+      "Do you have a nutrition plan?","Do you have any daily work out goals or routines?","Have you curated a way to deal with daily stress?", 4),
+  ];
 }
---
+++ src/app/models/categories-detail.model.ts
@@ -0,0 +1,3 @@
+export class categoriesDetail {
+  constructor (public title: string, public description: string, public question:[string], public id: number) { }
+}
