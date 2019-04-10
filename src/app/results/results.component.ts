e45ee8b8bb9f8045ee98faf580b844f714d15fe4
Nathan <nathanbn123@gmail.com>
2019-04-10 16:16:55 -0700

    convert data source to service

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -2,26 +2,34 @@ import { Component, Input,  OnInit, } from '@angular/core';
 import { AppComponent } from '../app.component'
 import { Assessment } from '../models/assessment.model'
 import { User } from '../models/user.model'
+import { ResultDataService } from '../result-data.service'
 
 
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
+  providers: [ResultDataService]
 })
 export class ResultsComponent implements OnInit {
-  @Input() childUserInfo: User[];
+   childUserInfo: User[];
 
   ngOnInit() {
-    // console.log(this.childUserInfo)
+    this.childUserInfo = this.resultDataService.UserList;
+
+    console.log(this.childUserInfo)
     // let length = this.childUserInfo[0].assessment.length - 1
     // console.log(this.childUserInfo[0].assessment[length].social)
   }
 
-  formatData(childUserInfo) {
+  constructor(private resultDataService: ResultDataService) {}
+
+
+  formatData() {
+    // console.log(this.childUserInfo[0].assessment.length - 1)
     let length = this.childUserInfo[0].assessment.length - 1
-    for (let i = 0; i > this.childUserInfo.length; i ++ )
+    for (let i = 0; i < this.childUserInfo.length; i ++ )
     if (this.childUserInfo[i].assessment[length].finance >= 8 ) {
       console.log('good')
       return "good-circle"
