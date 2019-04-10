d85b2222518bff93734e264ac5e100ecc78a51fe
Nathan <nathanbn123@gmail.com>
2019-04-10 15:02:54 -0700

    create broken function

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResultsComponent } from '../results/results.component';
 
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,  OnInit, } from '@angular/core';
 import { AppComponent } from '../app.component'
 import { Assessment } from '../models/assessment.model'
 import { User } from '../models/user.model'
@@ -10,12 +10,28 @@ import { User } from '../models/user.model'
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnInit {
   @Input() childUserInfo: User[];
 
-  formatData(childUserInfo) {
-    
+  ngOnInit() {
+    // console.log(this.childUserInfo)
+    // let length = this.childUserInfo[0].assessment.length - 1
+    // console.log(this.childUserInfo[0].assessment[length].social)
+  }
 
+  formatData(childUserInfo) {
+    let length = this.childUserInfo[0].assessment.length - 1
+    for (let i = 0; i > this.childUserInfo.length; i ++ )
+    if (this.childUserInfo[i].assessment[length].finance >= 8 ) {
+      console.log('good')
+      return "good-circle"
+    } else if (this.childUserInfo[i].assessment[length].finance >= 4 ) {
+      console.log('ok')
+      return "ok-circle"
+    } else {
+      console.log('bad')
+      return "bad-circle"
+    }
   }
 
 
